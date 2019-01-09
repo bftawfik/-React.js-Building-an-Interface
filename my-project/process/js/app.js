8a621b90eadf1927db50a3e7a7921056770ca86b
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-01-09 16:45:13 +0200

    end of lesson 4 chapter 2

--- my-project/process/js/app.js
+++ my-project/process/js/app.js
@@ -6,37 +6,23 @@ class MainInterface extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      data: [
-        {
-          "petName": "Buffy",
-          "ownerName": "Hassum Harrod",
-          "aptDate": "2016-06-20 15:30",
-          "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
-        },
-        {
-          "petName": "Spot",
-          "ownerName": "Constance Smith",
-          "aptDate": "2016-06-24 08:30",
-          "aptNotes": "This German Shepherd is having some back pain"
-        },
-        {
-          "petName": "Goldie",
-          "ownerName": "Barot Bellingham",
-          "aptDate": "2016-06-22 15:50",
-          "aptNotes": "This Goldfish has some weird spots in the belly"
-        },
-        {
-          "petName": "Mitten",
-          "ownerName": "Hillary Goldwyn",
-          "aptDate": "2016-06-21 9:15",
-          "aptNotes": "Cat has excessive hairballs"
-        }
-      ]
+      myAppointment: []
     };
   }
 
+  componentDidMount(){
+    this.serverRequest = $.get('./js/data.json', function(result){
+      var tempApts = result;
+      this.setState({myAppointment: tempApts});
+    }.bind(this));
+  }
+
+  componentWillUnmount(){
+    this.serverRequest.abort();
+  }
+
   render(){
-    const filteredApts = this.state.data.map((value, index) => {
+    const filteredApts = this.state.myAppointment.map((value, index) => {
       return(<li key={index} className="pet-item media">
         <div className="pet-head">
           <span className="pet-name">{value.petName}</span>
